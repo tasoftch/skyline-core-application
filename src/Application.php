abae87a62aa7a48c5f623dbe82a94cd28d992ada
Thomas Abplanalp <info@tasoft.ch>
2019-09-21 08:41:01 +0200

    FIX: Apply not modified responses

--- src/Application.php
+++ src/Application.php
@@ -141,11 +141,11 @@ class Application implements ApplicationInterface
 
                     $resp = $renderEvent->getResponse();
                 }
-                $response->prepare( $request );
-                if($response->isNotModified( $request ))
-                    $response->sendHeaders();
+                $resp->prepare( $request );
+                if($resp->isNotModified( $request ))
+                    $resp->sendHeaders();
                 else
-                    $response->send();
+                    $resp->send();
             } else {
                 $e = new ApplicationException("Application Launch Error", 500);
                 $e->setDetails("Application can not lauch because no service manager is available. Probably Skyline CMS did not bootstrap");
