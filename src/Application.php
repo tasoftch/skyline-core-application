b7e2500ece88acb5b838401afa624dcfe93b670b
Thomas Abplanalp <info@tasoft.ch>
2019-10-25 08:48:14 +0200

    FIX: Send headers before render

--- src/Application.php
+++ src/Application.php
@@ -147,7 +147,7 @@ class Application implements ApplicationInterface
                     $resp = $renderEvent->getResponse();
                 }
 
-                $resp->send();
+                $resp->sendContent();
             } else {
                 $e = new ApplicationException("Application Launch Error", 500);
                 $e->setDetails("Application can not lauch because no service manager is available. Probably Skyline CMS did not bootstrap");
